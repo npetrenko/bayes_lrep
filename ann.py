dbad523dbec5a5344572b69171769dd574c27bb5
Nikita Petrenko <npetrenko97@gmail.com>
2017-10-28 13:39:28 +0300

    updated variable scopes

--- ann.py
+++ ann.py
@@ -28,32 +28,34 @@ class ANN:
         normal_4bnn = ANN.normal_4bnn
         
         def create_bn(inp_length):
-            self.offsnscales.append([tf.Variable(normal(inp_length), name='mean_offset'), 
-                                     tf.Variable(normal(inp_length) + 1, name='std_scale')])
-            self.running_stats.append([tf.Variable(normal(inp_length), name='mean_sunning_stats'), 
-                                       tf.Variable(normal(inp_length) + 1, name='std_sunning_stats')])
+            self.offsnscales.append([tf.get_variable(initializer=normal(inp_length), name='mean_offset'), 
+                                     tf.get_variable(initializer=normal(inp_length) + 1, name='std_scale')])
+            self.running_stats.append([tf.get_variable(initializer=normal(inp_length), name='mean_sunning_stats'), 
+                                       tf.get_variable(initializer=normal(inp_length) + 1, name='std_sunning_stats')])
 
         with tf.variable_scope('layer_1'):
-            if not bayesian:
-                W, b = [tf.Variable(normal(input_shape, configuration[0]), name='W'), 
-                        tf.Variable(normal(configuration[0]), name='b')]
-            else:
-                W, b, kl = self.construct_bayes_weights(input_shape, configuration[0], nsamples)
-                self.loss += kl
-            self.weights.append([W,b])
-            if batchnorm:
-                create_bn(configuration[1])
-
-        for i, (inc, out) in enumerate(zip(configuration[:-1], configuration[1:])):
-            with tf.variable_scope('layer_' + str(i+2)):
+            with tf.name_scope('layer_1/'):
                 if not bayesian:
-                    W, b = tf.Variable(normal(inc, out), name='W'), tf.Variable(normal(out), name='b')
+                    W, b = [tf.get_variable(initializer=normal(input_shape, configuration[0]), name='W'), 
+                            tf.get_variable(initializer=normal(configuration[0]), name='b')]
                 else:
-                    W, b, kl = self.construct_bayes_weights(inc, out, nsamples)
+                    W, b, kl = self.construct_bayes_weights(input_shape, configuration[0], nsamples)
                     self.loss += kl
                 self.weights.append([W,b])
                 if batchnorm:
-                    create_bn(out)
+                    create_bn(configuration[1])
+
+        for i, (inc, out) in enumerate(zip(configuration[:-1], configuration[1:])):
+            with tf.variable_scope('layer_{}'.format(i+2)):
+                with tf.name_scope('layer_{}/'.format(i+2)):
+                    if not bayesian:
+                        W, b = tf.get_variable(initializer=normal(inc, out), name='W'), tf.get_variable(initializer=normal(out), name='b')
+                    else:
+                        W, b, kl = self.construct_bayes_weights(inc, out, nsamples)
+                        self.loss += kl
+                    self.weights.append([W,b])
+                    if batchnorm:
+                        create_bn(out)
 
     def __call__(self, *x, mode=None):
         x = tf.concat(x, axis=-1)
@@ -94,40 +96,44 @@ class ANN:
             return x
 
         for i, (W, b) in enumerate(self.weights[:-1]):
-            with tf.name_scope('layer_' + str(i+1)):
+            with tf.variable_scope('layer_{}'.format(i+1), reuse=True):
+                with tf.variable_scope('layer_{}/'.format(i+1)):
+                    if self.bayesian:
+                        if not self.lrep:
+                            x = batch_matmul(x, W) + b[:,tf.newaxis,:]
+                        else:
+                            x_mu = tf.matmul(x, W['mu']) + b['mu']
+                            with tf.name_scope('RP'):
+                                x_sigma = tf.matmul(x**2, W['std']**2) + b['std']**2
+                                x_sigma = tf.sqrt(x_sigma)
+                                x = tf.random_normal(tf.shape(x_mu))*x_sigma + x_mu
+                    else:
+                        x = tf.matmul(x, W) + b
+                    if self.batchnorm:
+                        x = apply_bn(x, self.offsnscales[i], self.running_stats[i], mode)
+                    x = tf.nn.relu(x)
+
+        W, b = self.weights[-1]
+        with tf.variable_scope('layer_{}'.format(len(self.weights)), reuse=True):
+            with tf.name_scope('layer_{}/'.format(len(self.weights))):
                 if self.bayesian:
                     if not self.lrep:
                         x = batch_matmul(x, W) + b[:,tf.newaxis,:]
                     else:
                         x_mu = tf.matmul(x, W['mu']) + b['mu']
-                        x_sigma = tf.matmul(x**2, W['std']**2) + b['std']**2
-                        x_sigma = tf.sqrt(x_sigma)
-                        x = tf.random_normal(tf.shape(x_mu))*x_sigma + x_mu
+                        with tf.name_scope('RP'):
+                            x_sigma = tf.matmul(x**2, W['std']**2) + b['std']**2
+                            x_sigma = tf.sqrt(x_sigma)
+                            x = tf.random_normal(tf.shape(x_mu))*x_sigma + x_mu
                 else:
                     x = tf.matmul(x, W) + b
+
                 if self.batchnorm:
-                    x = apply_bn(x, self.offsnscales[i], self.running_stats[i], mode)
-                x = tf.nn.relu(x)
+                    x = apply_bn(x, self.offsnscales[-1], self.running_stats[-1], mode)
+                self.logits = x
 
-        W, b = self.weights[-1]
-        with tf.name_scope('layer_' + str(len(self.weights))):
-            if self.bayesian:
-                if not self.lrep:
-                    x = batch_matmul(x, W) + b[:,tf.newaxis,:]
-                else:
-                    x_mu = tf.matmul(x, W['mu']) + b['mu']
-                    x_sigma = tf.matmul(x**2, W['std']**2) + b['std']**2
-                    x_sigma = tf.sqrt(x_sigma)
-                    x = tf.random_normal(tf.shape(x_mu))*x_sigma + x_mu
-            else:
-                x = tf.matmul(x, W) + b
-
-            if self.batchnorm:
-                x = apply_bn(x, self.offsnscales[-1], self.running_stats[-1], mode)
-            self.logits = x
-
-            if self.activation:
-                return self.activation(x)
+                if self.activation:
+                    return self.activation(x)
 
         return x
     
@@ -138,25 +144,26 @@ class ANN:
         bayes_prior_std = ANN.bayes_prior_std
         
         with tf.variable_scope('W'):
-            Wmean = tf.Variable(normal(in_dim, out_dim))
-            Wlogstd = tf.Variable(normal(in_dim, out_dim)-4)
+            Wmean = tf.get_variable('mean', initializer=normal(in_dim, out_dim))
+            Wlogstd = tf.get_variable('logstd', initializer=normal(in_dim, out_dim)-4)
             Wstd = tf.log1p(tf.exp(Wlogstd))
 
         kl += tf.reduce_sum(tf.log(bayes_prior_std/Wstd) + (Wstd**2 + Wmean**2)/(2*bayes_prior_std**2) - 0.5)
         
         with tf.variable_scope('b'):
-            bmean = tf.Variable(normal(out_dim))
-            blogstd = tf.Variable(normal(out_dim)-4)
+            bmean = tf.get_variable('mean', initializer=normal(out_dim))
+            blogstd = tf.get_variable('logstd', initializer=normal(out_dim)-4)
             bstd = tf.log1p(tf.exp(blogstd))
 
-        kl += tf.reduce_sum(tf.log(bayes_prior_std/bstd) + (bstd**2 + bmean**2)/(2*bayes_prior_std**2) - 0.5)
-        
-        
-        W_distr = normal_4bnn(nsamples, in_dim, out_dim)*Wstd + Wmean
-        b_distr = normal_4bnn(nsamples, out_dim)*bstd + bmean
+        with tf.name_scope('KL'):
+            kl += tf.reduce_sum(tf.log(bayes_prior_std/bstd) + (bstd**2 + bmean**2)/(2*bayes_prior_std**2) - 0.5)
         
         tf.summary.histogram('Wstd', Wstd)
         tf.summary.histogram('bstd', bstd)
+
+        if not self.lrep:
+            W_distr = normal_4bnn(nsamples, in_dim, out_dim)*Wstd + Wmean
+            b_distr = normal_4bnn(nsamples, out_dim)*bstd + bmean
         
         if not self.lrep:
             return W_distr, b_distr, kl
